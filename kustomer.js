56d67af5917a2ca1eab59cbbd0e631af3d321694
davidkthao <46228875+davidkthao@users.noreply.github.com>
2018-12-28 14:01:07 -0600

    Create kustomer.js

---
+++ kustomer.js
@@ -0,0 +1,8 @@
+<script id="kustomer">
+!function(a,b,c,d){a.Kustomer=c,c._q=[],c._i=[],c.init=function(a){function b(a,b){a[b]=function(){a._q.push([b].concat(Array.prototype.slice.call(arguments,0)))}}for(var d="init clear identify track start describe on".split(" "),e=0;e<d.length;e++)b(c,d[e]);c._i.push(a)};var e=b.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.kustomerapp.com/cw/sdk.v1.1.min.js";var f=b.getElementsByTagName("script")[0];f.parentNode.insertBefore(e,f)}(this,document,window.Kustomer||{});
+
+// Add your API key here
+Kustomer.init('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViZDBiNzJhYTUwMzY4MDAxZDI3ZTQwNCIsInVzZXIiOiI1YmQwYjcyYWYwMmE3ZjAwMTE2ZGQ1NTYiLCJvcmciOiI1YmIzNzRmMTdkMDVjNTAyNDA3ZGI4YzYiLCJvcmdOYW1lIjoiYmVkZHIiLCJ1c2VyVHlwZSI6Im1hY2hpbmUiLCJyb2xlcyI6WyJvcmciXSwiYXVkIjoidXJuOmNvbnN1bWVyIiwiaXNzIjoidXJuOmFwaSIsInN1YiI6IjViZDBiNzJhZjAyYTdmMDAxMTZkZDU1NiJ9.XK3ZeN8Dhq8S_5OEy2cLmRLowtiTRL4Eq3rTUg-07mo'); 
+
+ // Start the chat client
+Kustomer.start();</script> 
